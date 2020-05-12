267abd1175fe274e50b7f1746384ffda508ba664
David Jarvey <djarvey@outlook.com>
2020-05-12 16:30:15 +0000

    Add a new network module :fire: :hammer:

--- vnet.tf
+++ vnet.tf
@@ -1,19 +1,4 @@
-provider "azurerm" {
-  version = "=1.44.0"
-}
-
 module "network" {
   source  = "app.terraform.io/djarvey-training/network/azurerm"
   version = "3.0.1"
 }
-
-resource "azurerm_resource_group" "dbresourcegroup" {
-  name     = "${var.prefix}-workshop"
-  location = var.location
-}
-
-resource "azurerm_virtual_network" "vnetdb" {
-  name                = "${var.prefix}-vnetdb"
-  location            = azurerm_resource_group.dbresourcegroup.location
-  address_space       = [var.address_space]
-  resource_group_name = azurerm_resource_group.dbresourcegroup.name
