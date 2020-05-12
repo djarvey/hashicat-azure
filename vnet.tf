13e8b44c74b294aac4a0ff31e0c04174df94b432
David Jarvey <djarvey@outlook.com>
2020-05-12 16:26:53 +0000

    Add a new network module :fire: :hammer:

--- vnet.tf
+++ vnet.tf
@@ -7,13 +7,13 @@ module "network" {
   version = "3.0.1"
 }
 
-resource "azurerm_resource_group" "myresourcegroup" {
+resource "azurerm_resource_group" "dbresourcegroup" {
   name     = "${var.prefix}-workshop"
   location = var.location
 }
 
 resource "azurerm_virtual_network" "vnetdb" {
   name                = "${var.prefix}-vnetdb"
-  location            = azurerm_resource_group.myresourcegroup.location
+  location            = azurerm_resource_group.dbresourcegroup.location
   address_space       = [var.address_space]
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  resource_group_name = azurerm_resource_group.dbresourcegroup.name
